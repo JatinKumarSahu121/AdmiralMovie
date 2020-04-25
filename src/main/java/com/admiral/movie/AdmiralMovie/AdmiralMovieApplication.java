1f7137ea48085c41bec655392e59a3f3bf7a7fda
Jatin-App <jatin.sahu@appinventiv.com>
2020-04-25 16:34:55 +0530

    Initial Code Push

---
+++ src/main/java/com/admiral/movie/AdmiralMovie/AdmiralMovieApplication.java
@@ -0,0 +1,13 @@
+package com.admiral.movie.AdmiralMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmiralMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmiralMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admiral/movie/AdmiralMovie/AdmiralMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admiral.movie.AdmiralMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmiralMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
