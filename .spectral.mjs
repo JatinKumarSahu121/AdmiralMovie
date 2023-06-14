35ed6a72858083cc20be9792dcf38b0b608dcfbf
Jatin Kumar Sahu <nikhil@ility.com>
2023-06-14 17:13:27 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwNTgzNg/spectral.js?branch_slug=master&token=2a0a31b5-2029-4d74-b8cd-20b70095c995";
+export default { extends: ruleset };
